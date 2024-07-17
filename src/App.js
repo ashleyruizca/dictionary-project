9f5fdee35af0196861176cd3cb575409a34a7527
ashleyruizca <156567181+ashleyruizca@users.noreply.github.com>
2024-07-17 10:06:05 -0400

    integrated a search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello world
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>coded by shecodes</small>
+        </footer>
+      </div>
     </div>
   );
 }

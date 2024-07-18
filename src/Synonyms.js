f96f815f83cbfe78c9dcb6e7381e1c32b196c03a
ashleyruizca <156567181+ashleyruizca@users.noreply.github.com>
2024-07-18 11:17:32 -0400

    Made a component for synonym

---
+++ src/Synonyms.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.Synonyms) {
+    return (
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonyms, index) {
+          return <span key={index}>{synonyms}</span>;
+        })}
+      </ul>
+    );
+  } else {
+    return null;
+  }
+}
